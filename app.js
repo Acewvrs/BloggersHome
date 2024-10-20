0e6334c1e80e8d9a6a91cff5a287fa77f9eea158
Jun <song.junyoung64@gmail.com>
2024-10-20 07:08:06 -0400

    centered content on /home and added css

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const usersRouter = require("./routers/userRouter");
 
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/views')); // for using css
 app.use(express.urlencoded({ extended: true }));
 app.use("/", usersRouter);
 
